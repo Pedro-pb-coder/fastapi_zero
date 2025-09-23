0dca5b5e92bc84c6a4573d78e518681f1a83c264
Pedro-pb-coder <pedro.pe.madureira@gmail.com>
2025-09-22 21:26:34 -0300

    Aula 3 antes dos testes

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -2,11 +2,24 @@ from http import HTTPStatus
 
 from fastapi import FastAPI
 
-from fastapi_zero.schemas import Message
+from fastapi_zero.schemas import Message, UserDB, UserPublic, UserSchema
 
-app = FastAPI()
+app = FastAPI(title=' Meu projeto API')
+database = []
 
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá Mundo!'}
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(
+        username=user.username,
+        email=user.email,
+        password=user.password,
+        id=len(database) + 1
+    )
+    database.append(user_with_id)
+    return user_with_id
--- fastapi_zero/schemas.py
+++ fastapi_zero/schemas.py
@@ -3,3 +3,19 @@ from pydantic import BaseModel
 
 class Message(BaseModel):
     message: str
+
+
+class UserSchema(BaseModel):
+    username: str
+    email: str
+    password: str
+
+
+class UserPublic(BaseModel):
+    username: str
+    email: str
+    id: int
+
+
+class UserDB(UserSchema):
+    id: int
